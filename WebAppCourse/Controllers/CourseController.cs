30f56362f618f2a52d13eb1595c917a3b342ecc8
Niteshvarmaa <niteshvarma126@gmail.com>
2024-01-31 15:13:40 +0530

    Add project files.

---
+++ WebAppCourse/Controllers/CourseController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppCourse.Models;
+
+namespace WebAppCourse.Controllers
+{
+    public class CourseController : Controller
+    {
+        // GET: Course
+        static List<Course> courses = new List<Course>()
+        {
+        new Course { CourseId = 1,CourseName = "ASP.NET",CourseFee = 200,Technology = "C#",StartDate = DateTime.Now,EndDate = DateTime.Now.AddDays(10)},
+                new Course { CourseId = 2,CourseName = "Csharp",CourseFee = 200,Technology = "C#",StartDate = DateTime.Now,EndDate = DateTime.Now.AddDays(20)},
+                        new Course { CourseId = 3,CourseName = "C++",CourseFee = 200,Technology = "C programming",StartDate = DateTime.Now,EndDate = DateTime.Now.AddDays(30)}
+
+
+
+        };
+        public ActionResult Index()
+        {
+            return View(courses);
+        }
+    }
+}
---
+++ WebAppCourse/Models/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppCourse.Models
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public decimal CourseFee { get; set; }
+        public string Technology { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
